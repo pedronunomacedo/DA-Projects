8c102897f35a8b21d480cc76b2730b42e118a1f8
pedronunomacedo <up202007531@edu.fe.up.pt>
2022-04-07 20:01:52 +0100

    Added the file of the class Data

--- Tests/Data.cpp
+++ Tests/Data.cpp
@@ -2,4 +2,61 @@
 // Created by pedromacedo on 4/7/22.
 //
 
+#include <sstream>
 #include "Data.h"
+
+Data::Data() {
+
+}
+
+bool Data::readProducts(string filename) {
+    fstream file(filename);
+    string line;
+
+    if (!file.is_open()) {
+        cout << "File " << filename << " doesn't exist\n";
+        return false;
+    }
+
+    getline(file, line);
+    while (getline(file, line)) {
+        double weight, volume; int reward, duration;
+        istringstream iss(line);
+        iss >> volume >> weight >> reward >> duration;
+        Product p(weight, volume, reward, duration);
+        products.push_back(p);
+    }
+
+    return true;
+}
+
+bool Data::readCouriers(string filename) {
+    fstream file(filename);
+    string line;
+
+    if (!file.is_open()) {
+        cout << "File " << filename << " doesn't exist\n";
+        return false;
+    }
+
+    getline(file, line);
+    while (getline(file, line)) {
+        //string name, plate;
+        int maxVolume, maxWeight, cost;
+        istringstream iss(line);
+        iss >> maxVolume >> maxWeight >> cost;
+        Courier c(maxVolume, maxWeight, cost);
+        couriers.push_back(c);
+    }
+
+    return true;
+}
+
+const vector<Product> &Data::getProducts() {
+    return products;
+}
+
+const vector<Courier> &Data::getTrucks() {
+    return couriers;
+}
+
--- Tests/Data.h
+++ Tests/Data.h
@@ -5,9 +5,24 @@
 #ifndef UNTITLED_DATA_H
 #define UNTITLED_DATA_H
 
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include "Product.h"
+#include "Courier.h"
+using namespace std;
 
 class Data {
-
+private:
+    vector<Product> products;
+    vector<Courier> couriers;
+public:
+    Data();
+    bool readProducts(string filename);
+    bool readCouriers(string filename);
+    const vector<Product> &getProducts();
+    const vector<Courier> &getTrucks();
 };
 
 
