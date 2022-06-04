d7312f2b418257d8eba786503cef20c264bb30ec
gonzallito <goncaloferreira2408@gmail.com>
2022-06-04 11:05:20 +0100

    clean code

--- Classes/Scenario1.cpp
+++ Classes/Scenario1.cpp
@@ -17,14 +17,10 @@ void Scenario1::printpath(vector<int> &parent, int vertex, int target) {
 }
 
 int Scenario1::scenario1_1(vector<vector<tuple<int, int, int>>> &Graph, int src, int target) {
-    // To keep track of widest distance
     vector<int> widest(Graph.size(), INT_MIN);
 
-    // To get the path at the end of the algorithm
     vector<int> parent(Graph.size(), 0);
 
-    // Use of Minimum Priority Queue to keep track minimum
-    // widest distance vertex so far in the algorithm
     priority_queue<pair<int, int>, vector<pair<int, int> >, greater< > > container;
 
     container.push(make_pair(0, src));
@@ -40,22 +36,15 @@ int Scenario1::scenario1_1(vector<vector<tuple<int, int, int>>> &Graph, int src,
 
         for (auto vertex : Graph[current_src]) {
 
-            // Finding the widest distance to the vertex
-            // using current_source vertex's widest distance
-            // and its widest distance so far
             int distance = max(widest[get<1>(vertex)],
                                min(widest[current_src], get<0>(vertex)));
 
-            // Relaxation of edge and adding into Priority Queue
             if (distance > widest[get<1>(vertex)]) {
 
-                // Updating bottle-neck distance
                 widest[get<1>(vertex)] = distance;
 
-                // To keep track of parent
                 parent[get<1>(vertex)] = current_src;
 
-                // Adding the relaxed edge in the priority queue
                 container.push(make_pair(distance, get<1>(vertex)));
             }
         }
--- Classes/Scenario2.cpp
+++ Classes/Scenario2.cpp
@@ -22,52 +22,37 @@ void Scenario2::scenario2_1(vector<vector<int> > graph, int s, int t)
 {
     int u, j;
 
-    // Create a residual graph and fill the residual graph
-    // with given capacities in the original graph as
-    // residual capacities in residual graph
     vector<vector<int> > rGraph(graph.size()-1 , vector<int> (graph.size()-1)); // Residual graph where rGraph[i][j]
 
-
-
-    // indicates residual capacity of edge
-    // from i to j (if there is an edge. If
-    // rGraph[i][j] is 0, then there is not)
     for (u = 0; u < graph.size()-1; u++) {
         for (j = 0; j < graph.size()-1; j++) {
             rGraph[u][j] = graph[u][j];
         }
     }
 
-    vector<int> parent(graph.size()-1, 0); // This array is filled by BFS and to store path
+    vector<int> parent(graph.size()-1, 0);
 
-    int max_flow = 0; // There is no flow initially
+    int max_flow = 0;
     int numPeople = 6; // Ask the user for a number!!!!!!
 
-    // Augment the flow while there is path from source to
-    // sink
+
     while (bfs(rGraph, s, t, parent) && max_flow < numPeople) {
-        // Find minimum residual capacity of the edges along
-        // the path filled by BFS. Or we can say find the
-        // maximum flow through the path found.
+
         int path_flow = INT_MAX;
         for (j = t-1; j != s-1; j = parent[j]) {
             u = parent[j];
             path_flow = min(path_flow, rGraph[u][j]);
         }
 
-        // update residual capacities of the edges and
-        // reverse edges along the path
         for (j = t-1; j != s-1; j = parent[j]) {
             u = parent[j];
             rGraph[u][j] -= path_flow;
             rGraph[j][u] += path_flow;
         }
 
-        // Add path flow to overall flow
         max_flow += path_flow;
     }
 
-    // Return the overall flow
     cout << "The path is: "; printPath2(parent, s, t); cout << endl;
 }
 
@@ -75,33 +60,23 @@ void Scenario2::scenario2_1(vector<vector<int> > graph, int s, int t)
 
 
 
-/* Returns true if there is a path from source 's' to sink
-  't' in residual graph. Also fills parent[] to store the
-  path */
 bool Scenario2::bfs(vector<vector<int> > &rGraph, int s, int t, vector<int> &parent) {
-    // Create a visited array and mark all vertices as not
-    // visited
+
     bool visited[rGraph.size()];
     memset(visited, 0, sizeof(visited));
 
-    // Create a queue, enqueue source vertex and mark source
-    // vertex as visited
     queue<int> q;
     q.push(s-1);
     visited[s-1] = true;
     parent[s-1] = -1;
 
-    // Standard BFS Loop
     while (!q.empty()) {
         int u = q.front();
         q.pop();
 
         for (int v = 0; v < rGraph.size(); v++) {
             if (!visited[v] && rGraph[u][v] > 0) {
-                // If we find a connection to the sink node,
-                // then there is no point in BFS anymore We
-                // just have to set its parent and can return
-                // true
+
                 if (v == (t-1)) {
                     parent[v] = u;
                     return true;
@@ -112,8 +87,6 @@ bool Scenario2::bfs(vector<vector<int> > &rGraph, int s, int t, vector<int> &par
             }
         }
     }
-    // We didn't reach sink in BFS starting from source, so
-    // return false
     return false;
 }
 
@@ -121,68 +94,39 @@ bool Scenario2::bfs(vector<vector<int> > &rGraph, int s, int t, vector<int> &par
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-// 2.3
 int Scenario2::scenario2_3(vector<vector<int> > graph, int s, int t)
 {
     int u, j;
 
-    // Create a residual graph and fill the residual graph
-    // with given capacities in the original graph as
-    // residual capacities in residual graph
-    vector<vector<int> > rGraph(graph.size()-1 , vector<int> (graph.size()-1)); // Residual graph where rGraph[i][j]
+    vector<vector<int> > rGraph(graph.size()-1 , vector<int> (graph.size()-1));
 
-
-
-    // indicates residual capacity of edge
-    // from i to j (if there is an edge. If
-    // rGraph[i][j] is 0, then there is not)
     for (u = 0; u < graph.size()-1; u++) {
         for (j = 0; j < graph.size()-1; j++) {
             rGraph[u][j] = graph[u][j];
         }
     }
 
-    vector<int> parent(graph.size()-1, 0); // This array is filled by BFS and to store path
+    vector<int> parent(graph.size()-1, 0);
 
-    int max_flow = 0; // There is no flow initially
+    int max_flow = 0;
 
-    // Augment the flow while there is path from source to
-    // sink
     while (bfs(rGraph, s, t, parent)) {
-        // Find minimum residual capacity of the edges along
-        // the path filled by BFS. Or we can say find the
-        // maximum flow through the path found.
+
         int path_flow = INT_MAX;
         for (j = t-1; j != s-1; j = parent[j]) {
             u = parent[j];
             path_flow = min(path_flow, rGraph[u][j]);
         }
 
-        // update residual capacities of the edges and
-        // reverse edges along the path
         for (j = t-1; j != s-1; j = parent[j]) {
             u = parent[j];
             rGraph[u][j] -= path_flow;
             rGraph[j][u] += path_flow;
         }
 
-        // Add path flow to overall flow
         max_flow += path_flow;
     }
 
-    // Return the overall flow
     cout << "The path is: "; printPath2(parent, s, t); cout << endl;
     cout << "     The max flow is: ";
     return max_flow;
@@ -191,7 +135,6 @@ int Scenario2::scenario2_3(vector<vector<int> > graph, int s, int t)
 void Scenario2::printPath2(vector<int> &parent, int source, int target) {
     vector<int> path;
     for (int i = target; i > source; i = (parent[i-1]+1)) {
-        //cout << "--" << parent[i-1]+1;
         path.push_back(parent[i-1]+1);
         if(parent[i-1]+1 == source)
             break;
